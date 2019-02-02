8e3fc1b029239219e8759cb6ca65759e16b9c62f
sergevik <serg.oker@gmai.com>
2019-02-02 13:33:41 +0300

    bug bix

--- hello.py
+++ hello.py
@@ -13,4 +13,4 @@ def app(environ, start_response):
         ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    return iter(data)
+    return iter(data.encode('utf-8'))
