3b6c11fdfe2abb4efe2ae5e93b3bd3d6e5a13bf6
sie <serg.oker@gmail.com>
2019-02-23 13:09:55 +0000

    migrated to sqlite3

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -76,11 +76,8 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'demo',
-        'USER': 'box',
-        'PASSWORD' : 'demo',
-        'HOST' : 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
